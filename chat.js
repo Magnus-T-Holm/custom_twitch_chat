5f8c6a6e18b9b6fd816df959c6f327588cffe71e
Magnus Holm <magnusthestrup@hotmail.com>
2023-02-14 11:08:52 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,6 +1,8 @@
 var chat = document.querySelector("#chat>ul");
 
 ComfyJS.onChat = (user, message, flags, self, extra) => {
+    CreateChatText(message, user, extra.userColor, extra);
+
     var newMessage = document.createElement("li");
     var text = document.createElement("blockquote");
 
@@ -11,8 +13,13 @@ ComfyJS.onChat = (user, message, flags, self, extra) => {
     chat.append(newMessage);
 }
 
+async function CreateChatText(message, user, color, extra) { }
+
 ComfyJS.onMessageDeleted = (id, extra) => {
     console.log(id, extra);
     chat.innerHTML = "";
 };
+
+async function HttpAPICall(HttpCall, Twitch) { }
+
 ComfyJS.Init("AvaterVT");
